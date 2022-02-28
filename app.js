3994ec961dbed25ac003f6fba23525d09e4b6776
Farkas <fcsabi789@gmail.com>
2022-02-28 09:44:24 +0100

    ennyi ment

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var szamitGomb=document.getElementById("szamitGomb");
 var eredmenyIn=document.getElementById("eredmeny");
 var oldalIn=document.getElementById("oldal");
-szamitGomb.addEventListener('click',function(){ 
+szamitGomb.addEventListener('click',function(){ let oldalIn=oldalIn.value;
+    let minta=/^[0-9]+$/;
+    if(oldalIn.match(minta)){
+        console.log('oké')
 
+    }else{console.log('nem oké')
+    alert('hiba! csak szám adható meg ')
+return;
 };
+let oldal =Number(oldalIn.value);
+
